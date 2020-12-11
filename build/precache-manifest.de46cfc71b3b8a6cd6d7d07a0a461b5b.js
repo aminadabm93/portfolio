49ba885968dd785fcbc6b97ef7c8d09e5ce6dddd
aminadab.morales <aminadabm93@gmail.com>
2020-12-10 20:48:10 -0600

    updating git ignore

---
+++ build/precache-manifest.de46cfc71b3b8a6cd6d7d07a0a461b5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "541d3915e18064a14f04",
+    "url": "/portfolio/static/css/main.36223bb3.chunk.css"
+  },
+  {
+    "revision": "541d3915e18064a14f04",
+    "url": "/portfolio/static/js/main.541d3915.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "f9c77cd4501bfe6da91ae9e7f3c0f4b0",
+    "url": "/portfolio/index.html"
+  }
+];
