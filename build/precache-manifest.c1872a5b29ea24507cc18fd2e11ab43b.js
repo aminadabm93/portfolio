878f40750f8912e1c9200978752057f5153326d0
aminadab.morales <aminadabm93@gmail.com>
2020-12-15 00:45:22 -0600

    adding sushi project!

---
+++ build/precache-manifest.c1872a5b29ea24507cc18fd2e11ab43b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fb18d1e0e3a438453ff1",
+    "url": "/portfolio/static/css/main.36223bb3.chunk.css"
+  },
+  {
+    "revision": "fb18d1e0e3a438453ff1",
+    "url": "/portfolio/static/js/main.fb18d1e0.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portfolio/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "6c2ce5a0983e07300ab7613e44ee4016",
+    "url": "/portfolio/index.html"
+  }
+];
