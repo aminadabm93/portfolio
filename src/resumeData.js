50b3b7a314ca85d6a0571986a1865fc557812202
aminadab.morales <aminadabm93@gmail.com>
2021-01-07 16:22:36 -0600

    editing resume link

--- src/resumeData.js
+++ src/resumeData.js
@@ -18,7 +18,7 @@ let resumeData = {
         },
         {
           "name":"resume",
-          "url":"https://drive.google.com/file/d/1Al0H2awsGgRdp49tU0kYyaLm8mFA6yQB/view?usp=sharing",
+          "url":"https://drive.google.com/file/d/1Al0H2awsGgRdp49tU0kYyaLm8mFA6yQB/view",
           "className":"fa fa-file"
         }
       ],
