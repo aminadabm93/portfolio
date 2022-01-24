f05cc8c282d7fbf92417e98935aabc5773eecee8
moralesaatconed <aminadabmorales@salesforce.com>
2022-01-24 11:26:58 -0600

    Updates

--- precache-manifest.191708edf5287aad50d524e8bef5c1e2.js
+++ precache-manifest.191708edf5287aad50d524e8bef5c1e2.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "afcf4ceab6413d3cdbc4",
-    "url": "/portfolio/static/css/main.36223bb3.chunk.css"
+    "revision": "46994ff4844643078eb1",
+    "url": "/portfolio/static/css/main.f8246524.chunk.css"
   },
   {
-    "revision": "afcf4ceab6413d3cdbc4",
-    "url": "/portfolio/static/js/main.afcf4cea.chunk.js"
+    "revision": "46994ff4844643078eb1",
+    "url": "/portfolio/static/js/main.46994ff4.chunk.js"
   },
   {
     "revision": "6eb6405c9d99258624bc",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
   },
   {
-    "revision": "9e91d8da8db410310466a917e6ac8215",
+    "revision": "82b1f7b84594b63da600185d7f624145",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.6b41f21ead41d4e9feeb321309fec351.js"
+  "/portfolio/precache-manifest.191708edf5287aad50d524e8bef5c1e2.js"
 );
 
 workbox.clientsClaim();
